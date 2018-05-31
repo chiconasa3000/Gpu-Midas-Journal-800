1df2ff23569f764a10f0fb83e8b50c760ce09bab
chiconasa3000 <csucavelando@gmail.com>
2018-05-31 12:40:34 -0500

    iteration with step tolerance

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cris/Frameworks/Gpu-Midas-Journal-800
-# Build directory: /home/cris/Frameworks/Gpu-Midas-Journal-800/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
