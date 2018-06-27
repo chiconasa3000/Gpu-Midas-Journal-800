9f20b1474e8524f7c620570d47cea9b0fb1781b6
chiconasa3000 <csucavelando@gmail.com>
2018-06-27 08:56:46 -0500

    CorrectdynamicCast

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/citesoft01/Frameworks/Qt-Frameworks1/Gpu-Midas-Journal-800
-# Build directory: /home/citesoft01/Frameworks/Qt-Frameworks1/Gpu-Midas-Journal-800/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
